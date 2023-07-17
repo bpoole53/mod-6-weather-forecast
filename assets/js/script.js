4f5494e73d5d883170992cc3fdd0a622be2f1a7b
bpoole53 <bpoole53@gmail.com>
2023-07-16 22:52:42 -0500

    added fetch requests to convert the city name to latitude and longitude values and then put those into a fetch request for the 5 day weather forecast url

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+const apiKey = 'b457a582093a49542cd7b514701843e3';
+var cityName;
+var latitude;
+var longitude;
+
+$('#city-input').submit(function (event) {
+    event.preventDefault();
+    var input = $("#city-name").val();
+    console.log(input);
+    cityName = input;
+    console.log(cityName);
+    getCoordinates();
+});
+
+
+function getCoordinates () {
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`)
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+    latitude = data[0].lat;
+    longitude = data[0].lon;   
+    console.log(`${latitude}   ${longitude}`);     
+    getApi();
+    });    
+}
+
+function getApi() {
+    var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    console.log(requestUrl);
+    
+    fetch(requestUrl)
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+    console.log(data.city);
+    })
+}
