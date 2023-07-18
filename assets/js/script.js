35e395182eb8e7bb6f7ca6d9178962b30555be33
bpoole53 <bpoole53@gmail.com>
2023-07-18 00:52:34 -0500

    got the weather forecasts all filled out and styled

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,23 @@
+$(document).ready(function () {
+
 const apiKey = 'b457a582093a49542cd7b514701843e3';
-var container = $('#container')
+var fiveDay = document.getElementById('five-day-forecast')
+var container = document.getElementById('container')
 var currentWeather = document.getElementById('main-forecast')
+var mainFirstLine = document.getElementById('first-line forecast')
+var mainSecondLine = document.getElementById('second-line forecast')
+var mainThirdLine = document.getElementById('third-line forecast')
 var cityName;
 var latitude;
 var longitude;
 
 $('#city-input').submit(function (event) {
     event.preventDefault();
+    fiveDay.innerHTML = '';
     var input = $("#city-name").val();
-    console.log(input);
     cityName = input;
-    console.log(cityName);
     getCoordinates();
 });
-
 
 function getCoordinates () {
     fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`)
@@ -23,14 +27,13 @@ function getCoordinates () {
     .then(function (data) {
     latitude = data[0].lat;
     longitude = data[0].lon;   
-    console.log(`${latitude}   ${longitude}`);     
     getApi();
     });    
 }
 
 function getApi() {
     var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
-    console.log(requestUrl);
+   
     
     fetch(requestUrl)
     .then(function (response) {
@@ -38,21 +41,41 @@ function getApi() {
     })
     .then(function (data) {
     console.log(data);    
-    console.log(data.list[0].main.humidity);
-    console.log(data.list[0].main.temp);
-    console.log(data.list[0].wind.speed);
-    console.log(data.city.name);
-
+ 
+    var wind = data.list[0].wind.speed
+    var temp = data.list[0].main.temp
+    var humidity = data.list[0].main.humidity;
     var date = dayjs(data.list[0].dt_txt.split(' ')[0]).format('MM/DD/YYYY');
     var icon = data.list[0].weather[0].icon; 
-    currentWeather.innerHTML= `${cityName} ${date} <img src = 'https://openweathermap.org/img/wn/${icon}@2x.png' class= "weather-icon"></img>`;
+     
+    currentWeather.innerHTML= `${cityName} (${date}) <img src = 'https://openweathermap.org/img/wn/${icon}@2x.png' class= "weather-icon"></img>`;
+    mainFirstLine.textContent = `Temp: ${temp}°F`
+    mainSecondLine.textContent = `Wind: ${wind} MPH`
+    mainThirdLine.textContent = `Humidity: ${humidity}%`   
     
+    console.log(data.list.length)
+    for (x = 8; x < data.list.length; x++) {
+        if(x%8 === 0) {
+        var windFive = data.list[x].wind.speed;
+        console.log(x)
+        console.log(windFive)
+        var tempFive = data.list[x].main.temp;
+        var humidityFive = data.list[x].main.humidity;
+        var dateFive = dayjs(data.list[x].dt_txt.split(' ')[0]).format('MM/DD/YYYY');
+        var iconFive = data.list[x].weather[0].icon; 
+        $('.container-fiveday').append(`<div class='five-day-stats'>
+                                            <p class='forecast'>${dateFive}</p>
+                                            <p><img src = 'https://openweathermap.org/img/wn/${iconFive}@2x.png' class="fiveday-icon"></img></p>
+                                            <p class='forecast'>Temp: ${tempFive}°F</p>
+                                            <p class='forecast'>Wind: ${windFive} MPH</p>
+                                            <p class='forecast'>Humidity: ${humidityFive}%</p>
+                                        </div>`);
+        }                                                 
+    };
     
-
-    console.log(data.list[0].weather[0].icon)
-    console.log(data.list[0].dt_txt)
-    console.log(date);
+    $('#four-day').text('Four Day Forecast:')
+    container.style.border = '2px solid black'
     
     })
 }
-
+})
